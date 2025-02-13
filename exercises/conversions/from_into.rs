4556991f966015fc870ef455a9126693a178c973
LRL52 <525687841@qq.com>
2025-02-13 22:02:54 +0800

    solve conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Self::default();
+        }
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Self::default();
+        }
+        if v[0].is_empty() {
+            return Self::default();
+        }
+        let Ok(age) = v[1].parse() else {
+            return Self::default();
+        };
+        Self {
+            name: v[0].into(),
+            age,
+        }
     }
 }
 
